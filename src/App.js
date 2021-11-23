4e337394478e37d0db7d21278e7950108b00c91b
mugabe richard <mugabe.richard.37@gmail.com>
2021-11-24 00:27:41 +0200

    fetching the data from weather api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Weather from './components/Weather';
 import { WiDaySunny } from 'weather-icons-react';
 
-function App() {
+const API_KEY="694d861285b8d07599d35fe33b42ba8d"
+
+// api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid={API key}
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+   getWeather= async () =>{
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}
+    `);
+    const response = await api_call.json();
+    console.log(response);
+  };
+  rander(){
   return (
     <div className="App">
       <Weather />
@@ -11,5 +29,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,29 +1,29 @@
 import { WiDaySunny } from "weather-icons-react";
 const Weather = () => {
   return (
-    <div class="min-h-screen  mx-4 flex items-center justify-center">
-      <div class="flex border border-blue-500 rounded-3xl flex-col bg-white  p-4 w-full max-w-xs">
-        <div class="font-bold text-xl">Sydney</div>
-        <div class="text-sm text-gray-500">Thursday 10 May 2020</div>
-        <div class="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
+    <div className="min-h-screen  mx-4 flex items-center justify-center">
+      <div className="flex border border-blue-500 rounded-3xl flex-col bg-white  p-4 w-full max-w-xs">
+        <div className="font-bold text-xl">Sydney</div>
+        <div className="text-sm text-gray-500">Thursday 10 May 2020</div>
+        <div className="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
         <WiDaySunny size={500} color='#000' />
         </div>
-        <div class="flex flex-row items-center justify-center mt-6">
-          <div class="font-medium text-6xl">24°</div>
-          <div class="flex flex-col items-center ml-6">
+        <div className="flex flex-row items-center justify-center mt-6">
+          <div className="font-medium text-6xl">24°</div>
+          <div className="flex flex-col items-center ml-6">
             <div>Cloudy</div>
             {tempRange(20, 28)}
-            {/* <div class="mt-1">
-              <span class="text-sm">
-                <i class="far fa-long-arrow-up"></i>
+            {/* <div className="mt-1">
+              <span className="text-sm">
+                <i className="far fa-long-arrow-up"></i>
               </span>
-              <span class="text-sm font-light text-gray-500">28°C</span>
+              <span className="text-sm font-light text-gray-500">28°C</span>
             </div>
             <div>
-              <span class="text-sm">
-                <i class="far fa-long-arrow-down"></i>
+              <span className="text-sm">
+                <i className="far fa-long-arrow-down"></i>
               </span>
-              <span class="text-sm font-light text-gray-500">20°C</span>
+              <span className="text-sm font-light text-gray-500">20°C</span>
             </div> */}
           </div>
         </div>
@@ -41,16 +41,16 @@ function tempRange(min, max){
     return (
         <h3>
            <div>
-              <span class="text-sm">
-                <i class="far fa-long-arrow-down"></i>
+              <span className="text-sm">
+                <i className="far fa-long-arrow-down"></i>
               </span>
-              <span class="text-sm font-light text-gray-500">{min}°C</span>
+              <span className="text-sm font-light text-gray-500">{min}°C</span>
             </div>
-            <div class="mt-1">
-              <span class="text-sm">
-                <i class="far fa-long-arrow-up"></i>
+            <div className="mt-1">
+              <span className="text-sm">
+                <i className="far fa-long-arrow-up"></i>
               </span>
-              <span class="text-sm font-light text-gray-500">{max}°C</span>
+              <span className="text-sm font-light text-gray-500">{max}°C</span>
             </div>
         </h3>
     );
